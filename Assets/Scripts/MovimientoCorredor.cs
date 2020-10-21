e131b9a94e836b625ad012f9d828fe92e250627a
Luis Plata <www.luisplata@gmail.com>
2020-10-20 21:53:00 -0500

    Movimiento base del gato, falta que cambie de carretera

--- Assets/Scripts/MovimientoCorredor.cs
+++ Assets/Scripts/MovimientoCorredor.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class MovimientoCorredor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject puntoDondeDebeEstar;
+    public GameObject corredor;
+    private bool isLLego = true;
+    private Rigidbody2D rb;
+    [SerializeField] private int escalar;
+    public bool puedeVoltear;
+
+    private void Start()
     {
-        
+        CambiarDePunto(puntoDondeDebeEstar);
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void CambiarDePunto(GameObject posicion)
+    {
+        isLLego = false;
+        puntoDondeDebeEstar = posicion;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 diff = puntoDondeDebeEstar.transform.position - transform.position;
+        Vector2 llegando = puntoDondeDebeEstar.transform.position - transform.position;
+        if(llegando.sqrMagnitude <= 0.1 && !isLLego)
+        {
+            isLLego = true;
+        }
+        else
+        {
+            rb.velocity = diff * escalar;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("corredor"))
+        {
+            puedeVoltear = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("corredor"))
+        {
+            puedeVoltear = false;
+        }
     }
 }
--- Assets/Scripts/MovimientoPlayer.cs
+++ Assets/Scripts/MovimientoPlayer.cs
@@ -1,43 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MovimientoPlayer : MonoBehaviour
 {
     [SerializeField] InputManager inputManager;
     [SerializeField] MovimientoCorredor corredor;
     [SerializeField] float speed;
+    [SerializeField] private Button botonPanelDerecho, botonPanelIzquierdo;
+    [SerializeField] private List<GameObject> carriles;
+    [SerializeField] private int carrilSeleccionado;
+    [SerializeField] private GameObject corredorPista;
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        botonPanelDerecho.onClick.AddListener(delegate { CambiarDeCarrilHaciaLaDerecha(); });
+        botonPanelIzquierdo.onClick.AddListener(delegate { CambiarDeCarrilHaciaLaIzquierda(); });
+        corredor.CambiarDePunto(carriles[carrilSeleccionado]);
+        //transform.parent = corredorPista.transform;
+    }
+
+    private void CambiarDeCarrilHaciaLaDerecha()
+    {
+        carrilSeleccionado++;
+        if(carrilSeleccionado > carriles.Count -1)
+        {
+            carrilSeleccionado = carriles.Count - 1;
+            Debug.Log("Aqui es donde puede ir a otro direccion de la calle");
+        }
+        corredor.CambiarDePunto(carriles[carrilSeleccionado]);
+    }
+
+    private void CambiarDeCarrilHaciaLaIzquierda()
+    {
+        carrilSeleccionado--;
+        if (carrilSeleccionado < 0)
+        {
+            carrilSeleccionado = 0;
+            Debug.Log("Aqui es donde puede ir a otro direccion de la calle");
+        }
+        corredor.CambiarDePunto(carriles[carrilSeleccionado]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 vectorVelocidad = new Vector2();
-        if (inputManager.SeMovioHorizontalmente() != 0)
-        {
-            if(inputManager.SeMovioHorizontalmente() > 0)
-            {
-            }
-            else
-            {   
-            }
-            vectorVelocidad.x = inputManager.SeMovioHorizontalmente();
-        }
-        if(inputManager.SeMovioVerticalmente() != 0)
-        {
-            if(inputManager.SeMovioVerticalmente() > 0)
-            {   
-            }
-            else
-            {
-            }
-            vectorVelocidad.y = inputManager.SeMovioVerticalmente();
-        }
+        Vector2 vectorVelocidad = Vector2.up;
         rb.velocity = vectorVelocidad * (speed * Time.deltaTime);
+
     }
 }
