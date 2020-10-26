f8f20c7c3249fc8027154921cc3397954b243b68
Luis Plata <www.luisplata@gmail.com>
2020-10-25 21:29:37 -0500

    Se agrega el perro con un trigger, para colocar dificultad

---
+++ Assets/Scripts/v2/DiapradorDePerros.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiapradorDePerros : MonoBehaviourDeLaCasa
+{
+    [SerializeField] private GameObject perro;
+    protected override void AccionDeColisionConPlayer()
+    {
+        comenzarContarTiempoRespawn = true;
+    }
+
+    protected override void AccionDeLaCuentaRegresiva()
+    {
+        Debug.Log("Instanciamos al Perro");
+        Instantiate(perro, transform.parent.transform);
+    }
+}
---
+++ Assets/Scripts/v2/Perro.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perro : MonoBehaviourDeLaCasa
+{
+    [SerializeField] private GameObject target;
+    [SerializeField] private float speed;
+    Rigidbody2D rb;
+    [SerializeField] private bool comezarPerseguir;
+    [SerializeField] private float aumentoParametro;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (comezarPerseguir)
+        {
+            Vector2 diff = (target.transform.position - transform.position).normalized;
+            rb.velocity = diff * (speed * Time.deltaTime);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, (target.transform.localPosition - transform.position).normalized);
+    }
+
+    public void AumentarDificultad()
+    {
+        speed *= aumentoParametro;
+    }
+
+    protected override void AccionDeColisionConPlayer()
+    {
+        
+    }
+
+    protected override void AccionDeLaCuentaRegresiva()
+    {
+        
+    }
+}
